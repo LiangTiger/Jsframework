419888244602d2b7a096b858eddf729f6c35a67f
LiangTiger <740959030@qq.com>
2016-12-07 22:58:02 +0800

    1

---
+++ Array.js
@@ -0,0 +1,24 @@
+//jQuery//
+var makeArray=function(array){
+    var ret=[];
+    if(varray!=null){
+        var i=array.length;
+        if(i==null||typeof array==="string"||jQuery.isFunction(array)||array.setInterval)
+            ret[0]=array;
+        else
+            while (i)
+            ret(i=i-1)=array[i];
+      }
+      return ret;
+} 
+//Prototype.js//
+function $A(iterable){
+    if(!iterable)
+        return [];
+    if(iterable.toArray)
+        return iterable.toArray();
+        var length=iterable.length||0,result=new Array(length);
+        while(length=length-1)
+        results[length]=iterable[length];
+        return results;
+}
