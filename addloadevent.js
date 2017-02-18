d8567f75cdb650e297241a3c44a21c7cc2c4aebf
LiangTiger <740959030@qq.com>
2017-02-18 14:28:45 +0800

    11

--- addloadevent.js
+++ addloadevent.js
@@ -16,3 +16,16 @@ window.onload=function(){
     var txt=document.createTextNode('hello world');
     para.appendChild(txt);
 }
+window.onload=function(){
+    var para=document.createElement('p');
+    var txt1=document.createTextNode('This is');
+    para.appendChild(txt1);
+    var emphasis=document.createElement('em');
+    var txt2=document.createTextNode('my');
+    emphasis.appendchild(txt2);
+    para.appendchild(emphasis);
+    var txt3=document.createTextNode('content');
+    para.appendchild(txt3);
+    var testdiv=document.getElementById('testdiv');
+    testdiv.appendchild(para);
+}
